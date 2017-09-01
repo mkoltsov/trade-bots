c7f379fb282301809ddeec54af83e1d987b6bc9a
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2017-09-01 10:39:26 +0200

    updated

--- lib/ticker.rb
+++ lib/ticker.rb
@@ -44,8 +44,8 @@ end
 delay=3600
 loop do
 
-  btc_profit="BTC: #{calculate_position(update_price(get_bought(pairs[:bitcoin]))) - calculate_position(get_bought(pairs[:bitcoin]))}"
-  eth_profit="ETH: #{calculate_position(update_price(get_bought(pairs[:ethereum]))) - calculate_position(get_bought(pairs[:ethereum]))}"
+  btc_profit=calculate_position(update_price(get_bought(pairs[:bitcoin]))) - calculate_position(get_bought(pairs[:bitcoin]))
+  eth_profit=calculate_position(update_price(get_bought(pairs[:ethereum]))) - calculate_position(get_bought(pairs[:ethereum]))
   puts "#{btc_profit} #{eth_profit}"
   # binding.pry
   # open_orders=gdax.orders.select {|i| i['status']=='open'}.map {|i| "#{i["created_at"]} - #{i["product_id"]} - #{i["price"]} - #{i["size"]} "}
@@ -58,9 +58,9 @@ loop do
   # telegram_send(balance)
   case
     when btc_profit< -30, eth_profit < -30
-      telegram_send("#{btc_profit} #{eth_profit}")
+      telegram_send("BTC #{btc_profit} ETH #{eth_profit}")
     when btc_profit >= 50, eth_profit >= 50
-      telegram_send("#{btc_profit} #{eth_profit}")
+      telegram_send("BTC #{btc_profit} ETH#{eth_profit}")
   end
 
   # puts "------------------------------------------------------"
