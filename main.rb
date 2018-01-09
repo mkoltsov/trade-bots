cde089392335379d841f9c9b73e79c9db78819e5
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2018-01-09 14:15:37 +0100

    markdown added

--- main.rb
+++ main.rb
@@ -78,8 +78,8 @@ listen=-> {
           exit=false
           until exit do
             begin
-              msg = JSON.parse(HTTParty.get('https://api.coinmarketcap.com/v1/ticker/?limit=1500&convert=EUR').body).select {|e| arr.include?(e["id"])}.map {|el| "#{el['symbol']} - #{el["price_eur"]} - #{el["rank"]} - #{el["percent_change_1h"]}  - #{el["percent_change_24h"]}  - #{el["percent_change_7d"]}"}
-              bot.api.send_message(chat_id: message.chat.id, text: "#{msg}")
+              msg = JSON.parse(HTTParty.get('https://api.coinmarketcap.com/v1/ticker/?limit=1500&convert=EUR').body).select {|e| arr.include?(e["id"])}.map {|el| "*#{el['symbol']}* - #{el["price_eur"]} - #{el["rank"]} - #{el["percent_change_1h"]}  - #{el["percent_change_24h"]}  - #{el["percent_change_7d"]}"}
+              bot.api.send_message(chat_id: message.chat.id, text: "#{msg}", parse_mode: 'Markdown')
               exit=true
             rescue Exception => e
               bot.api.send_message(chat_id: message.chat.id, text: "got #{e}, will retry")
